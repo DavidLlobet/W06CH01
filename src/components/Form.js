4c550649edd221b0a142a8541496034ce8ae45d6
David <david.llobet6@gmail.com>
2021-11-01 17:00:18 +0100

    Create Form component (html structure)

---
+++ src/components/Form.js
@@ -0,0 +1,18 @@
+const Form = () => {
+  return (
+    <form onSubmit>
+      <label htmlFor></label>
+      <input
+        autoComplete="off"
+        type="text"
+        className="form-control"
+        id="id"
+        placeholder="id"
+        value
+        onChange
+      ></input>
+    </form>
+  );
+};
+
+export default Form;
