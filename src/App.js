0f670b5d58a81a9d18f575362f6eb361824ac6cf
David <david.llobet6@gmail.com>
2021-11-01 18:12:57 +0100

    Fix import Form in App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import "./App.css";
-import Form from "./components/Form";
+import Form from "./components/Form/Form";
 
 function App() {
   return (
     <>
       <h1>TODO LIST</h1>
-      <Form></Form>
+      <Form />
     </>
   );
 }
