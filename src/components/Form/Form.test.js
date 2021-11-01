2e4cced1df025aadfc3ee5eb5ace28b5c047ec39
David <david.llobet6@gmail.com>
2021-11-01 18:04:24 +0100

    Complement test with getByPlaceholderText

--- src/components/Form/Form.test.js
+++ src/components/Form/Form.test.js
@@ -1,10 +1,13 @@
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import Form from "./Form";
 
 describe("Given a Form component", () => {
   describe("When it renders", () => {
-    test("Then it should render", () => {
+    test("Then it should render an input", () => {
       render(<Form />);
+      const elementForm = screen.getByPlaceholderText("id");
+
+      expect(elementForm).toBeInTheDocument();
     });
   });
 });
