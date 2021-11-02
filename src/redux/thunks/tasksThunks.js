d219334a1d7314ac437f09f4d9652941d71f45fa
David <david.llobet6@gmail.com>
2021-11-02 21:03:52 +0100

    Refactor tasksThunks.js

--- src/redux/thunks/tasksThunks.js
+++ src/redux/thunks/tasksThunks.js
@@ -1,9 +1,11 @@
 import { loadTasksAction } from "../actions/actionCreators";
 
-const todoThunks = () => async (dispatch) => {
-  const response = await fetch("https://w06ch01-api--local.herokuapp.com/todo");
-  const todoList = await response.json();
-  dispatch(loadTasksAction(todoList));
+export const loadTasksThunk = () => {
+  return async (dispatch) => {
+    const response = await fetch(
+      "https://w06ch01-api--local.herokuapp.com/todo"
+    );
+    const tasks = await response.json();
+    dispatch(loadTasksAction(tasks));
+  };
 };
-
-export default todoThunks;
